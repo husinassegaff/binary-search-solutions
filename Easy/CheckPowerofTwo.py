9c69bc42f8bb992f3e171581a18a2788b23702d9
husinassegaff <husinassegaff15@gmail.com>
2021-11-19 23:23:40 +0700

    Solution of Check Power of Two (py)

---
+++ Easy/CheckPowerofTwo.py
@@ -0,0 +1,9 @@
+class Solution:
+    def solve(self, n):
+        if n == 0:
+            return False
+        while n > 1:
+            if n % 2 != 0:
+                return False
+            n = n // 2
+        return True
